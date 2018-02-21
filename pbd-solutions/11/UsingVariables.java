b6e92ea792d2e2b805d1d5bb1f1b60926abb00a7
tlui2 <36413954+tlui2@users.noreply.github.com>
2018-02-21 10:50:16 -0500

    Add files via upload

---
+++ pbd-solutions/11/UsingVariables.java
@@ -0,0 +1,10 @@
+public class UsingVariables {
+	public static void main(String[] args) {
+	String learn = "Computer Science";
+	int room = 113;
+	double euler = 2.71828;
+	
+	    System.out.println("This is room " + room + ".");
+	    System.out.println("e is close to " + euler + ".");
+	    System.out.println("I am learning a bit about " + learn + ".");}
+	    }
